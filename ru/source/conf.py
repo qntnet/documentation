47f5665059ea830a3b129877a92c20210623e05c
Dmitry Golovin <golovin@quantnet.ai>
2020-08-15 03:30:15 +0300

    minor fix

--- ru/source/conf.py
+++ ru/source/conf.py
@@ -53,6 +53,7 @@ html_theme = 'press'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+language='ru'
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
